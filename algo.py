ecc73d292cc01235a864aaadfa018c3e8a624e3d
showdeyang <showdeyang@gmail.com>
2021-04-20 17:38:43 +0800

    'css'

--- algo.py
+++ algo.py
@@ -407,6 +407,20 @@ def efficacy(trX, strat, T, thresholds, typeDefs=None, startIndex=0, endIndex=No
     
     return decisions, predZ, consumptions, risks
 
+def knnR(data, time_dimension=False):
+    t1 = time.time()
+    print('Regressing data... This may take a while...')
+    trX = knnRegress([data[feature] for feature in  data])
+    
+    if not time_dimension:
+        trX = trX[:,:,1]
+    
+    print('Regressing time:', time.time()-t1)
+    print('Encoding time dimension?', time_dimension)
+    print('data shape', trX.shape)
+    return trX
+
+
 
 class analysis:
     def __init__(self, data, inputVars, controlVars, outputVars, thresholds, typeDefs=None, safety=0.7, startIndex=None, endIndex=None, verbose=True):
@@ -485,29 +499,35 @@ class analysis:
         return res
 
 
+
+
+
 if __name__ == '__main__':
     # plt.style.use('dark_background')
     import json
     import time
     data = readJsonData(path / 'JunoProject' / '示例项目' / 'value.json')
     
-    inputVars = ['二沉池混合后-TP (mg/L)', '二沉池混合后-SS (mg/L)']
-    controlVars = ['高效澄清池-PAC(投加量) (mg/L)', '高效澄清池-PAM(投加量) (mg/L)']
-    outputVars = ['排放池-TP在线 (mg/L)', '高效澄清池-SS (mg/L)']
+    # inputVars = ['二沉池混合后-TP (mg/L)', '二沉池混合后-SS (mg/L)']
+    # controlVars = ['高效澄清池-PAC(投加量) (mg/L)', '高效澄清池-PAM(投加量) (mg/L)']
+    # outputVars = ['排放池-TP在线 (mg/L)', '高效澄清池-SS (mg/L)']
     
-    # inputVars = ['二沉池混合后-TOC (mg/L)', '缺氧池B（D-N）-NO3-N (mg/L)']
-    # controlVars = ['高效澄清池-粉炭(投加量) (mg/L)']
-    # outputVars = ['高效澄清池-TOC (mg/L)']
+    # # inputVars = ['二沉池混合后-TOC (mg/L)', '缺氧池B（D-N）-NO3-N (mg/L)']
+    # # controlVars = ['高效澄清池-粉炭(投加量) (mg/L)']
+    # # outputVars = ['高效澄清池-TOC (mg/L)']
     
-    typeDefs = [-1]*len(inputVars) + [1/len(controlVars)]*len(controlVars) + [2]*len(outputVars)
+    # typeDefs = [-1]*len(inputVars) + [1/len(controlVars)]*len(controlVars) + [2]*len(outputVars)
     
     
-    t1 = time.time()
+    # t1 = time.time()
     
-    thresholds = [{'q99%': 0.5}, {'q99%': 9, 'q80%': 7}]
-    A = analysis(data, inputVars, controlVars, outputVars, thresholds, typeDefs, safety=0.5, verbose=True)
-    print(A.risks)
-    print(A.consumptions)
-    print('time taken', time.time()-t1)
+    # thresholds = [{'q99%': 0.5}, {'q99%': 9, 'q80%': 7}]
+    # A = analysis(data, inputVars, controlVars, outputVars, thresholds, typeDefs, safety=0.5, verbose=True)
+    # print(A.risks)
+    # print(A.consumptions)
+    # print('time taken', time.time()-t1)
 
+    ##########################
+    
+    trX = knnR(data)
     
